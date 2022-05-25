372381b695c8d81ce59480abe6c3903e4bdb93ad
lawnshogan <Shawnlogan.gis@gmail.com>
2022-05-25 13:29:25 -0600

    Module 10.5.1

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ from flask_pymongo import PyMongo
 # To use the scraping code, we will convert from Jupyter notebook to Python.
 import scraping
 
+# Set up Flask
+app = Flask(__name__)
+
+# Tell Python how to connect to Mongo using PyMongo
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
