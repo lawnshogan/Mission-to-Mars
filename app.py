37e69bcdf95417a3879ced21db42567608bfecc1
lawnshogan <Shawnlogan.gis@gmail.com>
2022-05-25 13:48:45 -0600

    Module 10.5.1

--- app.py
+++ app.py
@@ -9,6 +9,28 @@ import scraping
 app = Flask(__name__)
 
 # Tell Python how to connect to Mongo using PyMongo
+# tells Python that our app will connect to Mongo using a URI, 
+# a uniform resource identifier similar to a URL.
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
+# Set up Flask routes
+
+# Define route for HTML page
+@app.route("/")
+def index():
+# uses PyMongo to find the "mars" collection in our database
+   mars = mongo.db.mars.find_one()
+# tells Flask to return an HTML template using an index.html file
+   return render_template("index.html", mars=mars)
+
+# Set up scraping route
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   return redirect('/', code=302)
+
+if __name__ == "__main__":
+   app.run()
